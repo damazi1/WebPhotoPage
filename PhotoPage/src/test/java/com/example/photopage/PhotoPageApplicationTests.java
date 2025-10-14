fea173365fc74d3720d7b4f8f1a09e715ae1438d
damazi1 <114773869+damazi1@users.noreply.github.com>
2025-10-14 20:51:25 +0200

    Utworzenie projektu Java Spring Boot

---
+++ PhotoPage/src/test/java/com/example/photopage/PhotoPageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.photopage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhotoPageApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
